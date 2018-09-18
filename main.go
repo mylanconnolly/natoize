9f015aa74aaa308405270d2f0816ec921899c0ad
Mylan Connolly <mconnolly@iotrak.com>
2018-09-17 20:53:03 -0400

    Added faster version

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"unicode"
 )
 
 var natoLetters = map[rune]string{
@@ -47,12 +48,12 @@ var natoLetters = map[rune]string{
 
 func main() {
 	for _, arg := range os.Args[1:] {
-		fmt.Println(Phoenetic(arg))
+		fmt.Println(PhoneticFast(arg))
 	}
 }
 
-// Phoenetic is the abstraction of the natoize logic.
-func Phoenetic(str string) (line string) {
+// Phonetic is the abstraction of the natoize logic.
+func Phonetic(str string) (line string) {
 	for _, r := range strings.ToLower(str) {
 		nato, found := natoLetters[r]
 
@@ -64,4 +65,88 @@ func Phoenetic(str string) (line string) {
 		line += " "
 	}
 	return line
+}
+
+func PhoneticFast(str string) string {
+	builder := strings.Builder{}
+
+	for _, s := range str {
+		switch unicode.ToLower(s) {
+		case 'a':
+			builder.WriteString("alfa ")
+		case 'b':
+			builder.WriteString("bravo ")
+		case 'c':
+			builder.WriteString("charlie ")
+		case 'd':
+			builder.WriteString("delta ")
+		case 'e':
+			builder.WriteString("echo ")
+		case 'f':
+			builder.WriteString("foxtrot ")
+		case 'g':
+			builder.WriteString("golf ")
+		case 'h':
+			builder.WriteString("hotel ")
+		case 'i':
+			builder.WriteString("india ")
+		case 'j':
+			builder.WriteString("juliet ")
+		case 'k':
+			builder.WriteString("kilo ")
+		case 'l':
+			builder.WriteString("lima ")
+		case 'm':
+			builder.WriteString("mike ")
+		case 'n':
+			builder.WriteString("november ")
+		case 'o':
+			builder.WriteString("oscar ")
+		case 'p':
+			builder.WriteString("papa ")
+		case 'q':
+			builder.WriteString("quebec ")
+		case 'r':
+			builder.WriteString("romeo ")
+		case 's':
+			builder.WriteString("sierra ")
+		case 't':
+			builder.WriteString("tango ")
+		case 'u':
+			builder.WriteString("uniform ")
+		case 'v':
+			builder.WriteString("victor ")
+		case 'w':
+			builder.WriteString("whiskey ")
+		case 'x':
+			builder.WriteString("xray ")
+		case 'y':
+			builder.WriteString("yankee ")
+		case 'z':
+			builder.WriteString("zulu ")
+		case '1':
+			builder.WriteString("one ")
+		case '2':
+			builder.WriteString("two ")
+		case '3':
+			builder.WriteString("three ")
+		case '4':
+			builder.WriteString("four ")
+		case '5':
+			builder.WriteString("five ")
+		case '6':
+			builder.WriteString("six ")
+		case '7':
+			builder.WriteString("seven ")
+		case '8':
+			builder.WriteString("eight ")
+		case '9':
+			builder.WriteString("nine ")
+		case '0':
+			builder.WriteString("zero ")
+		default:
+			builder.WriteString(string(s) + " ")
+		}
+	}
+	return builder.String()
 }
